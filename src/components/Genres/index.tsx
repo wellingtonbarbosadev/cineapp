e1bf68f383c3ce0c7b05621c0cab5cb2e154c370
Wellington Barbosa <huehueb58@gmail.com>
2026-06-03 23:41:16 -0300

    feat: add Genres component with styles and integrate into Home page

---
+++ src/components/Genres/index.tsx
@@ -0,0 +1,25 @@
+import styles from "./styles.module.css";
+
+export function Genres() {
+  return (
+    <div className={styles.hero}>
+      <ul>
+        <button>
+          <li className={styles.allGenre}>Todos</li>
+        </button>
+        <button>
+          <li>Ação</li>
+        </button>
+        <button>
+          <li>Drama</li>
+        </button>
+        <button>
+          <li>Sci-Fi</li>
+        </button>
+        <button>
+          <li>Terror</li>
+        </button>
+      </ul>
+    </div>
+  );
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
+import { Genres } from "../../components/Genres";
 import { Hero } from "../../components/Hero";
 
 export function Home() {
   return (
     <div>
       <Hero />
+      <Genres />
     </div>
   );
 }
